f741036073cf52a72cfc76b4b4d0f4689c738647
pbrumblay <peter@tyrconsulting.com>
2013-12-18 10:55:16 -0700

    Initial version.

---
+++ SemverCommentLabeller/SemverCommentLabeller.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+using ThoughtWorks.CruiseControl.Core.Label;
+using ThoughtWorks.CruiseControl.Core;
+
+namespace SemverCommentLabeller
+{
+
+    /// <summary>
+    /// Provides a valid System.Version label for your .NET assemblies that could be used to set the AssemblyVersionAttribute(). It increments
+    /// the version number using semver's rules on every successful integration by parsing comments for known prefixes. The CruiseControl.NET change number
+    /// is also used to set the 4th component of the .Net version number (since this isn't necessarily meaningful for semver). The change number/revision
+    /// is provided by source control systems like Subversion.
+    /// </summary>
+    /// <see cref="http://www.semver.org"/>
+    /// <title>Semver Comment Labeller</title>
+    [ReflectorType("semverCommentLabeller")]
+    public class SemverCommentLabeller
+        : LabellerBase
+    {
+        #region public properties
+        /// <summary>
+        /// Major number component of the version. Incremented when a comment is prefixed with the case-insensitive string "major:". 
+        /// </summary>
+        [ReflectorProperty("major", Required = false)]
+        public int Major { get; set; }
+
+        /// <summary>
+        /// Minor number component of the version.  Set to 0 when Major is incremented, 
+        /// otherwise incremented when a comment is prefixed with the case-insenstive string "minor:".
+        /// </summary>
+        [ReflectorProperty("minor", Required = false)]
+        public int Minor { get; set; }
+
+        /// <summary>
+        /// Patch number component of the version. Set to 0 when Major or Minor are incremented, 
+        /// otherwise incremented when a comment is prefixed with the case-insensitive string "patch:". 
+        /// </summary>
+        [ReflectorProperty("patch", Required = false)]
+        public int Patch { get; set; }
+
+        /// <summary>
+        /// Revision number component of the version. If not specified the revision number is the LastChangeNumber, provided by some VCS (e.g.
+        /// the svn revision with the Subversion task).
+        /// </summary>
+        [ReflectorProperty("revision", Required = false)]
+        public int Revision { get; set; }
+
+        /// <summary>
+        /// .NET Version number components are UInt16 and this causes problems if the LastChangeNumber exceeds 65535. By default SemverCommentLabeller
+        /// will set this value to LastChangeNumber % 10000 to keep the last 4 significant digits. Set this value to mod by something else.
+        /// </summary>
+        [ReflectorProperty("revisionModulus", Required = false)]
+        public int RevisionModulusValue { get; set; }
+
+
+        /// <summary>
+        /// A format applied to the major part of the buildnumber. 
+        /// </summary>
+        /// <version>1.7</version>
+        /// <default>0</default>
+        [ReflectorProperty("majorLabelFormat", Required = false)]
+        public string MajorLabelFormat { get; set; }
+
+
+        /// <summary>
+        /// A format applied to the minor part of the buildnumber. 
+        /// </summary>
+        [ReflectorProperty("minorLabelFormat", Required = false)]
+        public string MinorLabelFormat { get; set; }
+
+
+        /// <summary>
+        /// A format applied to the patch part of the buildnumber. 
+        /// </summary>
+        [ReflectorProperty("patchLabelFormat", Required = false)]
+        public string PatchLabelFormat { get; set; }
+
+
+        /// <summary>
+        /// A format applied to the revision part of the buildnumber. 
+        /// </summary>
+        [ReflectorProperty("revisionLabelFormat", Required = false)]
+        public string RevisionLabelFormat { get; set; }
+
+
+        #endregion
+
+        const string PATCH_PREFIX = "PATCH:";
+        const string MINOR_PREFIX = "MINOR:";
+        const string MAJOR_PREFIX = "MAJOR:";
+
+        #region ILabeller Members
+
+        /// <summary>
+        /// Generates the specified integration result.	
+        /// </summary>
+        /// <param name="integrationResult">The integration result.</param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public override string Generate(IIntegrationResult integrationResult)
+        {
+            Version oldVersion;
+            int major, minor, patch, revision;
+
+            // try getting old version
+            try
+            {
+                Log.Debug(string.Concat("[semverCommentLabeller] Old build label is: ", integrationResult.LastIntegration.Label));
+                oldVersion = new Version(integrationResult.LastIntegration.Label);
+                major = oldVersion.Major;
+                minor = oldVersion.Minor;
+                patch = oldVersion.Build;
+            }
+            catch (Exception)
+            {
+                oldVersion = new Version(0, 0, 0, 0);
+                major = minor = patch = 0;
+            }
+
+
+            Log.Debug(string.Concat("[semverCommentLabeller] Old version is: ", oldVersion.ToString()));
+
+            VersionNumberChange change = VersionNumberChange.None;
+            if (integrationResult.HasModifications())
+            {
+                foreach (var mod in integrationResult.Modifications)
+                {
+                    if (String.IsNullOrWhiteSpace(mod.Comment))
+                    {
+                        continue;
+                    }
+                    var comment = mod.Comment.ToUpperInvariant();
+
+                    if (comment.StartsWith(MAJOR_PREFIX))
+                    {
+                        change = VersionNumberChange.Major;
+                        break;
+                    }
+                    else if (comment.StartsWith(MINOR_PREFIX))
+                    {
+                        change = VersionNumberChange.Minor;
+                    }
+                    else if (comment.StartsWith(PATCH_PREFIX) && change == VersionNumberChange.None)
+                    {
+                        change = VersionNumberChange.Patch;
+                    }
+                }
+            }
+
+            switch (change)
+            {
+                case VersionNumberChange.Major:
+                    major++;
+                    minor = patch = 0;
+                    break;
+                case VersionNumberChange.Minor:
+                    minor++;
+                    patch = 0;
+                    break;
+                case VersionNumberChange.Patch:
+                    patch++;
+                    break;
+            }
+
+            //Calculate revision
+            if (int.TryParse(integrationResult.LastChangeNumber, out revision))
+            {
+                Log.Debug(
+                    string.Format(System.Globalization.CultureInfo.CurrentCulture, "[semverCommentLabeller] LastChangeNumber retrieved: {0}",
+                    revision));
+
+                int modVal = RevisionModulusValue > 0 ? RevisionModulusValue : 10000;
+                revision %= modVal;
+            }
+            else
+            {
+                Log.Debug("[semverCommentLabeller] LastChangeNumber of source control is '{0}', set revision number to '0'.",
+                          string.IsNullOrEmpty(integrationResult.LastChangeNumber)
+                              ? "N/A"
+                              : integrationResult.LastChangeNumber);
+            }
+
+            // use the revision from last build,
+            // because LastChangeNumber is 0 on ForceBuild or other failures
+            if (revision <= 0) revision = oldVersion.Revision;
+
+
+            Log.Debug(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                                    "[semverCommentLabeller] Major: {0} Minor: {1} Patch: {2} Revision: {3}", major, minor, patch, revision));
+
+            string result = string.Concat(major.ToString(MajorLabelFormat), ".", minor.ToString(MinorLabelFormat), ".", patch.ToString(PatchLabelFormat), ".", revision.ToString(RevisionLabelFormat));
+
+            Log.Debug(string.Concat("[semverCommentLabeller] New version is: ", result));
+
+            // return new version string
+            return result;
+        }
+
+        #endregion
+
+        enum VersionNumberChange
+        {
+            None = 0,
+            Patch = 1,
+            Minor = 2,
+            Major = 3
+        }
+    }
+
+    
+}
---
+++ SemverCommentLabeller/SemverCommentLabellerTests/SemverCommentLabellerTestFixture.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThoughtWorks.CruiseControl.Core;
+
+namespace SemverCommentLabellerTests
+{
+    [TestClass]
+    public class SemverCommentLabellerTestFixture
+    {
+        SemverCommentLabeller.SemverCommentLabeller _sut = new SemverCommentLabeller.SemverCommentLabeller();
+        IntegrationResultStub _integrationResult = new IntegrationResultStub();
+
+        [TestInitialize]
+        public void Setup()
+        {
+        }
+
+        //2.3.5.1239
+        [TestMethod]
+        public void TestNoPreviousBuild()
+        {
+            _integrationResult._last = null;
+            _integrationResult.Modifications = new Modification[] { new Modification { Comment = "Major: a major change" } };
+            string versionResult = _sut.Generate(_integrationResult);
+            Assert.AreEqual("1.0.0.1240", versionResult);
+        }
+
+        [TestMethod]
+        public void TestIncrementMajor()
+        {
+            _integrationResult.Modifications = new Modification[] { new Modification { Comment = "Major: a major change" } };
+            string versionResult = _sut.Generate(_integrationResult);
+            Assert.AreEqual("3.0.0.1240", versionResult);
+        }
+
+        [TestMethod]
+        public void TestIncrementMinor()
+        {
+            _integrationResult.Modifications = new Modification[] { new Modification { Comment = "minor: a minor change" } };
+            string versionResult = _sut.Generate(_integrationResult);
+            Assert.AreEqual("2.4.0.1240", versionResult);
+        }
+        
+        [TestMethod]
+        public void TestIncrementPatch()
+        {
+            _integrationResult.Modifications = new Modification[] { new Modification { Comment = "patch: a bug fix" } };
+            string versionResult = _sut.Generate(_integrationResult);
+            Assert.AreEqual("2.3.6.1240", versionResult);
+        }
+
+        [TestMethod]
+        public void TestIncrementRevisionOverflow()
+        {
+            _integrationResult._lastChangeNumber = "74444";
+
+            _integrationResult.Modifications = new Modification[] { new Modification { Comment = "patch: a bug fix" } };
+            string versionResult = _sut.Generate(_integrationResult);
+            Assert.AreEqual("2.3.6.4444", versionResult);
+        }
+    }
+
+    public class IntegrationResultStub : IIntegrationResult
+    {
+        public void AddTaskResult(ITaskResult result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTaskResult(string result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ArtifactDirectory
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string BaseFromArtifactsDirectory(string pathToBase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string BaseFromWorkingDirectory(string pathToBase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ThoughtWorks.CruiseControl.Remote.BuildCondition BuildCondition
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Guid BuildId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string BuildLogDirectory
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ThoughtWorks.CruiseControl.Core.Util.BuildProgressInformation BuildProgressInformation
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IIntegrationResult Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime EndTime
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Exception ExceptionResult
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Failed
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public System.Collections.ArrayList FailureTasks
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public System.Collections.ArrayList FailureUsers
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Fixed
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool HasModifications()
+        {
+            return true;
+        }
+
+        public bool HasSourceControlError
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public System.Collections.IDictionary IntegrationProperties
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ThoughtWorks.CruiseControl.Remote.IntegrationRequest IntegrationRequest
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsInitial()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Label
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ThoughtWorks.CruiseControl.Remote.IntegrationStatus LastBuildStatus
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string _lastChangeNumber = "1240";
+        public string LastChangeNumber
+        {
+            get { return _lastChangeNumber; }
+        }
+
+        public IntegrationSummary _last  = new IntegrationSummary(ThoughtWorks.CruiseControl.Remote.IntegrationStatus.Success, "2.3.5.1239", "2.3.5.1239", DateTime.Now.AddDays(-1));
+        public IntegrationSummary LastIntegration
+        {
+            get {
+                return _last;
+            }
+        }
+
+        public ThoughtWorks.CruiseControl.Remote.IntegrationStatus LastIntegrationStatus
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public DateTime LastModificationDate
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string LastSuccessfulIntegrationLabel
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void MarkEndTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkStartTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Merge(IIntegrationResult value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Modification[] Modifications { get; set; }
+
+        public System.Collections.Generic.List<ThoughtWorks.CruiseControl.Remote.NameValuePair> Parameters
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string ProjectName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string ProjectUrl
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool ShouldRunBuild()
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Collections.Generic.List<ThoughtWorks.CruiseControl.Remote.NameValuePair> SourceControlData
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Exception SourceControlError
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public DateTime StartTime
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ThoughtWorks.CruiseControl.Remote.IntegrationStatus Status
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Succeeded
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string TaskOutput
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public System.Collections.IList TaskResults
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public TimeSpan TotalIntegrationTime
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string WorkingDirectory
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
